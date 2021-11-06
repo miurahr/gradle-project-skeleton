e096670ccc9a58db92e228906397b018d3278c74
Hiroshi Miura <miurahr@linux.com>
2021-11-06 16:02:09 +0900

    Add gradle
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+group = "tokyo.northside"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
